f831efb53220e99e1c96d1e12eafb0498092dec9
s179054 <s179054@students.aie.edu.au>
2017-10-18 11:56:24 -0700

    Add experiments in SFW main

--- X-SFW-Test/Main.cpp
+++ X-SFW-Test/Main.cpp
@@ -4,6 +4,7 @@
 #include"vec3.h"
 #include "Player.h"
 #include"Transform.h"
+#include<cmath>
 int main()
 {
 	sfw::initContext();
@@ -15,20 +16,44 @@ int main()
 	//me.pos = {400, 300};
 
 	transform myTransform;
-	myTransform.position = vec2{ 300,400 };
-	myTransform.dimension = vec2{1,1};
+	myTransform.position = vec2{ 350,400 };
+	myTransform.dimension = vec2{40,2};
+	//myTransform.angle = 0;
 
+	transform myBaby;
+	myBaby.position = vec2{ 30,50 };
+	myBaby.dimension = vec2{ 40,40 };
+	//myBaby.angle = 0;
+	myBaby.e_parent = &myTransform;
 
+	transform myAbs[200];
+	for (int i = 0; i < 200; i++)
+	{
+		myAbs[i].position = vec2{ (float)30+i,(float)50+i };
+		myAbs[i].dimension = vec2{ 1,1 };
+		//myBaby.angle = 0;
 
+		myAbs[i].e_parent = &myTransform;
+	}
 
 	while (sfw::stepContext())
 	{
 		//me.update();
 		//me.draw();
 		float t = sfw::getDeltaTime();
-		myTransform.angle += 90 *  sfw::getDeltaTime();
-		myTransform.dimension = vec2{ sinf(t) + 20, sinf(t) + 1 };
-		DrawMatrix(myTransform.getLocalTransform(), 40);
+		myTransform.angle += 9 *  sfw::getDeltaTime();
+		myTransform.dimension = vec2{ sinf(t) + 5, sinf(t) + 1 };
+
+		DrawMatrix(myTransform.getGlobalTransform(), 40);
+		DrawMatrix(myBaby.getGlobalTransform(), 40);
+
+		for (int i = 0; i < 200; i++)
+		{
+			myAbs[i].dimension = vec2{ sinf(t +i) + 0, sinf(t + i) + 0 };
+			myAbs[i].position = vec2{ sinf(t * i) * i,sinf(t * i) * i };
+			DrawMatrix(myAbs[i].getGlobalTransform(), 40 + i);
+			//myAbs[i].angle += sinf(t) + 5 + i * sfw::getDeltaTime();
+		}
 
 
 
--- X-SFW-Test/Transform.h
+++ X-SFW-Test/Transform.h
@@ -7,12 +7,16 @@ class transform
 {
 public:
 	vec2 position;
+
+	//The size/scale
 	vec2 dimension;
 	float angle;
+	transform *e_parent;
 
 	transform();
 
 	mat3 getLocalTransform() const;
+	mat3 getGlobalTransform() const;
 
 private:
 
